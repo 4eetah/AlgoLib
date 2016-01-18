a2ff2578bd784f7d3c28062e9e4933b9110baa4b
Denis Romanchuk <den4ikkss@gmail.com>
2016-01-19 01:33:15 +0200

    Add Edge

---
+++ Edge.h
@@ -0,0 +1,30 @@
+#include <stdexcept>
+#include <string>
+#include <sstream>
+
+class Edge
+{
+    const int v;
+    const int w;
+    const double Weight;
+public:
+    Edge(int v, int w, double weight) : v(v), w(w), Weight(weight)
+    {
+        if (v < 0 || w < 0) 
+            throw std::out_of_range("Vertex name must be a nonnegative integer");
+    }
+    double weight() const { return Weight; }
+    int either() const { return v; }
+    int other(int vertex) const
+    {
+        if      (vertex == v) return w;
+        else if (vertex == w) return v;
+        else throw std::invalid_argument("Illegal endpoint"); 
+    }
+    std::string toString()
+    {
+        std::stringstream ss;        
+        ss << v << "-" << w << " " << Weight;
+        return ss.str(); 
+    }
+};
---
+++ clients/Edge_cli.cpp
@@ -0,0 +1,19 @@
+#include "../Edge.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    if (argc != 4)
+    {
+        cerr << "Using: ./main v w weight\n";
+        exit(1);
+    }
+    int v = std::stoi(argv[1]);
+    int w = std::stoi(argv[2]);
+    double weight = std::stod(argv[3]);
+    Edge e(v, w, weight);
+    std::cout << e.toString() << std::endl;
+}
