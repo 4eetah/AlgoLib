f60bf372dccbb7b22db934dbe161484c844d7f66
Denis Romanchuk <den4ikkss@gmail.com>
2016-01-20 03:01:09 +0200

    Add DirectedEdge

---
+++ DirectedEdge.h
@@ -0,0 +1,25 @@
+#include <stdexcept>
+#include <string>
+#include <sstream>
+
+class DirectedEdge
+{
+    const int v;
+    const int w;
+    const double Weight;
+public:
+    DirectedEdge(int v, int w, double weight) : v(v), w(w), Weight(weight)
+    {
+        if (v < 0 || w < 0) 
+            throw std::out_of_range("Vertex name must be a nonnegative integer");
+    }
+    double weight() const { return Weight; }
+    int from() const     { return v; }
+    int to() const       { return w; }
+    std::string toString()
+    {
+        std::stringstream ss;        
+        ss << v << "->" << w << " " << Weight;
+        return ss.str(); 
+    }
+};
